4436fc7c07fde9040b43c3514635c7185d11cba6
DonOndeje <donondeje@gmail.com>
2018-01-19 18:46:25 +0300

    removed certain comments

--- src/java/com/transport/Main.java
+++ src/java/com/transport/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.transport;
 
 import java.io.IOException;
@@ -21,10 +17,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author JQHN
- */
+
 public class Main extends HttpServlet {
 
     UserDB user = new UserDB();
@@ -84,28 +77,7 @@ public class Main extends HttpServlet {
             out.println("<font color=red>Either user name or password is wrong.</font>");
             dis.include(request, response);
         }
-//        Authentication authen = new Authentication();
-//        String hashedPass = null;
-//        try {
-//            hashedPass = authen.hash(password);
-//        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//
-//        String test = "1000:41a4be983bbd87de067c1ca6b8867e3c:18094eac13314139fc49486f898dab8ee207b18dbd9313ef6267296e71bca635f05014bf84fd7b338dd581349df894f3272509c06ac23ffa8b77c103b2cdecb8";
-//        String result = "No match";
-//        try {
-//            if(authen.validatePassword(password, test)){
-//                result = "matched";
-//            }
-//        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        response.setContentType("text/html");
-//
-//        try (PrintWriter out = response.getWriter()) {
-//            out.print(email + hashedPass + "\n" + result + "\n" + source);
-//        }
+
     }
 
     public int register(String username, String password, String contact, String email) throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
